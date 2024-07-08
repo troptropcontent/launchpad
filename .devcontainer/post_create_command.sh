673cdf9d4c3f6b505057375889739658fe3ecee6
Tom <tomecrepont@gmail.com>
2024-07-08 18:47:51 +0000

    first commit

---
+++ .devcontainer/post_create_command.sh
@@ -0,0 +1,4 @@
+# Set the default branch name to main
+git config --global init.defaultBranch ${GIT_DEFAULT_BRANCH}
+git config --global user.email ${GIT_USER_EMAIL}
+git config --global user.name ${GIT_USER_NAME}
